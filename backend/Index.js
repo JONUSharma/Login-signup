59d0080ef3c843389221d5b60398322c0021f314
JONUSharma <jonusharam4440@gmail.com>
2025-05-10 00:42:53 +0530

    some fixes

--- backend/Index.js
+++ backend/Index.js
@@ -18,7 +18,12 @@ ConnectMongoDb(URL)
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
-app.use(cors());
+app.use(cors(
+    {
+        origin : ["https://login-signup-auth-hwiw.onrender.com"],
+        credentials : true
+    }
+));
 
 //routes
 app.use("/",route)
