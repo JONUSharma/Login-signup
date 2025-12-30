09863353c09423c91d407911b802cb15531b2b66
Jonu Sharma <jonusharam4440@gmail.com>
2025-12-30 10:54:54 +0530

    few changes in index.js

--- backend/Index.js
+++ backend/Index.js
@@ -13,12 +13,16 @@ const URL = process.env.DATABASE
 //Connection with mongodb
 ConnectMongoDb(URL)
 
+app.use(cors({
+    origin : "*",
+}));
+
+
 //Midleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(cors());
 
 
 //routes
